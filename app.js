4b95927340d7267bbaf50aeeaf152aa6c0106b65
Liomka <goulityda@gmail.com>
2015-07-25 01:56:11 +0200

    Major fixes

--- app.js
+++ app.js
@@ -11,31 +11,36 @@ var express = require('express'),
 var routes = require('./routes/index'),
     users = require('./routes/users');
 
+
 // i18n initialization
 i18n.init({
-  saveMissing: true,
-  debug: true
+    saveMissing: true,
+    debug: true,
+    ignoreRoutes: ['images/', 'public/', 'css/', 'js/']
 });
 
 var app = express();
 
+
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use('/lib', express.static(__dirname + '/bower_components'));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(i18n.handle);
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(i18n.handle);
+
+i18n.registerAppHelper(app);
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/lib', express.static(__dirname + '/bower_components'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,8 +48,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-i18n.registerAppHelper(app);
 
 // error handlers
 
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-var projects = [
-  {
-    name: "1",
-    title: "myFirstTtiel",
-    anchor_name: "an anchor name for project",
-    content: "my content<b> test </b>",
-    sub_projects: [
-      {
-        anchor_name: "an anchar name for subproject",
-        title: "title subproject",
-        content: "content subproject"
-      }
-    ],
-    tags: []
-  },
-  {
-    name: "2",
-    title: "Olaaaa",
-    sub_projects: [
-      {
-        anchor_name: "an anchar name for subproject",
-        title: "title subproject",
-        content: "content subproject"
-      }
-    ],
-    tags: []
-  }];
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Liomka.IO', projects: projects });
+
+    var projects = [
+        {
+            name: "1",
+            title: "myFirstTtiel",
+            anchor_name: "an anchor name for project",
+            content: "my content<b> test </b>",
+            sub_projects: [
+                {
+                    anchor_name: "an anchar name for subproject",
+                    title: "title subproject",
+                    content: "content subproject"
+                }
+            ],
+            tags: []
+        },
+        {
+            name: "2",
+            title: "Olaaaa",
+            sub_projects: [
+                {
+                    anchor_name: "an anchar name for subproject",
+                    title: "title subproject",
+                    content: "content subproject"
+                }
+            ],
+            tags: []
+        }];
+
+    res.render('index', { title: 'Liomka.IO', projects: projects });
 });
 
 router.get('/cv', function (req, res, next) {
