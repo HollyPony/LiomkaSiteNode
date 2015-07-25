ca1e48bc3905c62430d36fc35798c8955df0d707
Liomka <goulityda@gmail.com>
2015-07-25 11:52:33 +0200

    Waypoints version 2 to 3

--- public/javascripts/cv.js
+++ public/javascripts/cv.js
@@ -12,48 +12,48 @@ $(document).ready(function () {
         return;
 
     // Init Vars
-    var entriesDetails = $('#cv').find('li').find('.entry-details');
+    var entriesDetails = $('#cv').find('li .entry-details');
 
     // Init DOM
     entriesDetails.css({ opacity: '0', left: '50%'});
+    
+    entriesDetails.each( function() {
+        var entry = $(this);
 
-    entriesDetails.waypoint({
-        handler: function(direction) {
-            if (direction == 'down')
-                $(this).css({opacity: '0', left: '50%'})
-        },
-        offset: function() {
-            return -$(this).height();
-        }
-    });
+        new Waypoint({
+            element: entry,
+            handler: function (direction) {
+                if (direction == 'down')
+                    entry.css({opacity: '0', left: '50%'})
+            },
+            offset: -entry.height()
+        });
 
-    entriesDetails.waypoint({
-        handler: function(direction) {
-            if (direction == 'up')
-                $(this).animate({left: '0%', opacity: '1'}, 1000, 'easeOutExpo');
-        },
-        offset: function() {
-            return -($(this).height() - 40 );
-        }
-    });
+        new Waypoint({
+            element: entry,
+            handler: function (direction) {
+                if (direction == 'up')
+                    entry.animate({left: '0%', opacity: '1'}, 1000, 'easeOutExpo');
+            },
+            offset: -(entry.height() - 40 )
+        });
 
-    entriesDetails.waypoint({
-        handler: function (direction) {
-            if (direction == 'down')
-                $(this).animate({left: '0%', opacity: '1'}, 1000, 'easeOutExpo');
-        },
-        offset: function () {
-            return $(window).height() - 40;
-        }
-    });
+        new Waypoint({
+            element: entry,
+            handler: function (direction) {
+                if (direction == 'down')
+                    entry.animate({left: '0%', opacity: '1'}, 1000, 'easeOutExpo');
+            },
+            offset: $(window).height() - 40
+        });
 
-    entriesDetails.waypoint({
-        handler: function (direction) {
-            if (direction == 'up')
-                $(this).css({opacity: '0', left: '50%'})
-        },
-        offset: function () {
-            return $(window).height();
-        }
+        new Waypoint({
+            element: entry,
+            handler: function (direction) {
+                if (direction == 'up')
+                    entry.css({opacity: '0', left: '50%'})
+            },
+            offset: $(window).height()
+        });
     });
 });
