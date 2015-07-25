8d2627873333b1ba43ceaf4f6e5b3a90b5befd83
Liomka <goulityda@gmail.com>
2015-07-25 04:25:01 +0200

    Init grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        // Task configuration.
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            bootstrap: {
+                files: {
+                    'public/lib/bootstrap/bootstrap.css': ['bower_components/bootstrap/dist/css/bootstrap.min.css'],
+                    'public/lib/bootstrap/bootstrap.js': ['bower_components/bootstrap/dist/js/bootstrap.min.js']
+                }
+            },
+            jquery: {
+                files: {
+                    'public/lib/jquery/jquery.js': ['bower_components/jquery/dist/jquery.min.js'],
+                    'public/lib/jquery/jquery-ui.js': ['bower_components/jquery-ui/jquery-ui.min.js']
+                }
+            },
+            stellar: {
+                files: {
+                    'public/lib/stellar/stellar.js': ['bower_components/stellar/src/jquery.stellar.js']
+                }
+            },
+            waypoints: {
+                files: {
+                    'public/lib/waypoints/waypoints.js': ['bower_components/waypoints/lib/jquery.waypoints.min.js']
+                }
+            }
+
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    jQuery: true
+                }
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib_test: {
+                src: ['lib/**/*.js', 'test/**/*.js']
+            }
+        },
+        qunit: {
+            files: ['test/**/*.html']
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib_test: {
+                files: '<%= jshint.lib_test.src %>',
+                tasks: ['jshint:lib_test', 'qunit']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+};
